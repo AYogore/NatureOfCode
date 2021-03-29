23d17c0ebe6a7a3b2557fcf21b03d1b3e0a15a52
AYogore <58088654+AYogore@users.noreply.github.com>
2021-03-28 23:15:53 -0500

    Basic Firefly movement using sine and cosine

---
+++ Assets/Animals/Butterfly/Butterfly.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Butterfly : MonoBehaviour
+{
+    // Variables for the location and speed of mover
+    private Vector2 location = new Vector2(0F, 0F);
+    private Vector2 velocity = new Vector2(0.01F, 0.01F);
+
+    // Variables to limit the mover within the screen space
+    private Vector2 minimumPos, maximumPos;
+
+    // A Variable to represent our mover in the scene
+    private GameObject mover;
+
+    public ButterflyObj butterfly1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // We want to start by setting the camera's projection to Orthographic mode
+        Camera.main.orthographic = true;
+
+        // Next we grab the minimum and maximum position for the screen
+        minimumPos = Camera.main.ScreenToWorldPoint(Vector2.zero);
+        maximumPos = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+
+        // We now can set the mover as a primitive sphere in unity
+        mover = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+
+        butterfly1 = new ButterflyObj();
+        //We need to create a new material for WebGL
+        Renderer r = mover.GetComponent<Renderer>();
+        r.material = new Material(Shader.Find("Diffuse"));
+    }
+
+    // Update is called once per frame forever and ever (until you quit).
+    void Update()
+    {
+        // Each frame, we will check to see if the mover has touched a boarder
+        // We check if the X/Y position is greater than the max position OR if it's less than the minimum position
+        bool xHitBorder = location.x > maximumPos.x || location.x < minimumPos.x;
+        bool yHitBorder = location.y > maximumPos.y || location.y < minimumPos.y;
+
+        // If the mover has hit at all, we will mirror it's speed with the corrisponding boarder
+
+        if (xHitBorder)
+        {
+            velocity.x = -velocity.x;
+        }
+
+        if (yHitBorder)
+        {
+            velocity.y = -velocity.y;
+        }
+
+        // Lets now update the location of the mover
+        location += velocity;
+
+        // Now we apply the positions to the mover to put it in it's place
+        mover.transform.position = new Vector2(location.x, location.y);
+    }
+
+    private void FixedUpdate()
+    {
+        //walker.step();
+        butterfly1.step();
+        butterfly1.CheckEdges();
+    }
+
+    public class ButterflyObj
+    {
+        // The basic properties of a mover class
+        private Vector3 location;
+
+        // The window limits
+        private Vector2 minimumPos, maximumPos;
+
+        // Gives the class a GameObject to draw on the screen
+        public GameObject butterfly1 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+
+        public ButterflyObj()
+        {
+            findWindowLimits();
+            location = Vector2.zero;
+            //We need to create a new material for WebGL
+            Renderer r = butterfly1.GetComponent<Renderer>();
+            r.material = new Material(Shader.Find("Diffuse"));
+        }
+
+        public void step()
+        {
+            location = butterfly1.transform.position;
+            //Each frame choose a new Random number 0,1,2,3, 
+            //If the number is equal to one of those values, take a step
+            int choice = Random.Range(0, 10);
+            if (choice <= 3)
+            {
+                location.x++;
+
+            }
+            else if (choice == 4)
+            {
+                location.x--;
+            }
+            else if (choice == 5)
+            {
+                location.y++;
+            }
+            else
+            {
+                location.y--;
+            }
+
+            butterfly1.transform.position += location * Time.deltaTime;
+        }
+
+        public void CheckEdges()
+        {
+            location = butterfly1.transform.position;
+
+            if (location.x > maximumPos.x)
+            {
+                location = Vector2.zero;
+            }
+            else if (location.x < minimumPos.x)
+            {
+                location = Vector2.zero;
+            }
+            if (location.y > maximumPos.y)
+            {
+                location = Vector2.zero;
+            }
+            else if (location.y < minimumPos.y)
+            {
+                location = Vector2.zero;
+            }
+            butterfly1.transform.position = location;
+        }
+
+        private void findWindowLimits()
+        {
+            // We want to start by setting the camera's projection to Orthographic mode
+            Camera.main.orthographic = true;
+            // Next we grab the minimum and maximum position for the screen
+            minimumPos = Camera.main.ScreenToWorldPoint(Vector2.zero);
+            maximumPos = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        }
+
+    }
+        
+
+}
---
+++ Assets/Animals/Firefly/Firefly.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Firefly : MonoBehaviour
+{//We need to create a walker
+    FireflyObj firefly1;
+    private float t;
+    public float Radius;
+    public float Speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        firefly1 = new FireflyObj();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {        //Have the walker choose a direction
+        //firefly1.step();
+        firefly1.CheckEdges();
+
+
+        t += Time.deltaTime;
+        firefly1.Rotation(t, Radius, Speed);
+        firefly1.step();
+        
+    }
+
+}
+
+
+public class FireflyObj
+{
+    // The basic properties of a mover class
+    private Vector3 location;
+    private Vector3 rotation;
+
+    //rotate parameters
+
+    
+    public float Radius;
+    public float Speed;
+
+    // The window limits
+    private Vector2 minimumPos, maximumPos;
+
+    // Gives the class a GameObject to draw on the screen
+    public GameObject firefly1 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+
+    public FireflyObj()
+    {
+        findWindowLimits();
+        location = Vector2.zero;
+        rotation = Vector2.zero;
+        //We need to create a new material for WebGL
+        Renderer r = firefly1.GetComponent<Renderer>();
+        r.material = new Material(Shader.Find("Diffuse"));
+    }
+
+    public void step()
+    {
+        location = firefly1.transform.position;
+        //Each frame choose a new Random number 0,1,2,3, 
+        //If the number is equal to one of those values, take a step
+        int choice = Random.Range(0, 4);
+        if (choice == 0)
+        {
+            location.x++;
+
+        }
+        else if (choice == 1)
+        {
+            location.x--;
+        }
+        else if (choice == 3)
+        {
+            location.y++;
+        }
+        else
+        {
+            location.y--;
+        }
+
+        firefly1.transform.position += location * Time.deltaTime;
+    }
+
+    public void CheckEdges()
+    {
+        location = firefly1.transform.position;
+
+        if (location.x > maximumPos.x)
+        {
+            location = Vector2.zero;
+        }
+        else if (location.x < minimumPos.x)
+        {
+            location = Vector2.zero;
+        }
+        if (location.y > maximumPos.y)
+        {
+            location = Vector2.zero;
+        }
+        else if (location.y < minimumPos.y)
+        {
+            location = Vector2.zero;
+        }
+        firefly1.transform.position = location;
+    }
+
+    public void Rotation(float t, float radius, float speed)
+    {
+        
+        Radius = radius;
+        Speed = speed;
+        rotation = firefly1.transform.position;
+        float radiusY = (float)(Radius *
+                      (0.5f + 0.5f * (
+                           Mathf.Sin(t * 0.3f) +
+                           0.3 * Mathf.Sin(2 * t + 0.8f) +
+                           0.26 * Mathf.Sin(3 * t + 0.8f))));
+
+
+        rotation = new Vector2(Radius * Mathf.Cos(t * Speed), radiusY * Mathf.Sin(t * Speed));
+       
+
+
+        firefly1.transform.position = rotation;
+    }
+    
+    
+
+    private void findWindowLimits()
+    {
+        // We want to start by setting the camera's projection to Orthographic mode
+        Camera.main.orthographic = true;
+        // Next we grab the minimum and maximum position for the screen
+        minimumPos = Camera.main.ScreenToWorldPoint(Vector2.zero);
+        maximumPos = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+    }
+}
